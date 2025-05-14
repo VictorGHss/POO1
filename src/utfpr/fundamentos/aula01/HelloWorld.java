0084a14bbf87f7779cfd2762f132821e8572df9d
VictorGHss <vg.hss0101@gmail.com>
2025-05-14 12:09:56 -0300

    Alterações em nomes de pastas

--- src/utfpr/fundamentos/aula01/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package utfpr.fundamentos.aula01;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
--- src/utfpr/fundamentos/aula04/Matriz.java
+++
@@ -1,5 +0,0 @@
-package utfpr.fundamentos.aula04;
-
-public class Matriz {
-    final int NUMERO_MAXIMO = 100;
-}
